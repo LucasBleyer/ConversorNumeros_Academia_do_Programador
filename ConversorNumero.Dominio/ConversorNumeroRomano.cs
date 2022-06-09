de922bebbfe8cb57211e64a62c202e452ce92630
Lucas Bleyer <lucasbleyer19@gmail.com>
2022-06-09 13:28:56 -0300

    Add project files.

---
+++ ConversorNumero.Dominio/ConversorNumeroRomano.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConversorNumero.Dominio
+{
+    public class ConversorNumeroRomano
+    {
+        Dictionary<string, int> numeros = new Dictionary<string, int>();
+
+        public ConversorNumeroRomano()
+        {
+            string[] unidadesRomanas = { "", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX" };
+
+            ConfigurarUnidadesRomanas(unidadesRomanas);
+
+            string[] dezenasRomanas = { "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC" };
+
+            ConfigurarDezenasRomanas(dezenasRomanas);
+
+            string[] centenasRomanas = { "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM" };
+
+            ConfigurarCentenasRomanas(centenasRomanas, dezenasRomanas);
+
+            string[] milhares = { "M", "MM", "MMM", "ĪV̄", "V̄", "V̄Ī", "V̄ĪĪ", "V̄ĪĪĪ", "ĪX̄" };
+        }
+       
+        public int ConverterParaArabico(string numeroRomano)
+        {
+            string numeroEsquerda = ObterNumeroEsquerda(numeroRomano);
+
+            string numeroDireita = ObterNumeroDireita(numeroRomano, numeroEsquerda);
+
+            return numeros[numeroEsquerda] + numeros[numeroDireita];
+        }
+
+        private static string ObterNumeroDireita(string numeroRomano, string numeroEsquerda)
+        {
+            return numeroRomano.Substring(numeroEsquerda.Length);
+        }
+
+        private static string ObterNumeroEsquerda(string numeroRomano)
+        {
+            return numeroRomano
+                            .Replace("IX", "")
+                            .Replace("VIII", "")
+                            .Replace("VII", "")
+                            .Replace("VI", "")
+                            .Replace("IV", "")
+                            .Replace("V", "")
+                            .Replace("III", "")
+                            .Replace("II", "")
+                            .Replace("I", "");
+        }
+
+        private void ConfigurarDezenasRomanas(string[] dezenasRomanas)
+        {
+            for (int i = 0; i < dezenasRomanas.Length; i++)
+            {
+                numeros.Add(dezenasRomanas[i], (i + 1) * 10);
+            }
+        }
+
+        private void ConfigurarUnidadesRomanas(string[] unidadesRomanas)
+        {
+            for (int i = 0; i < unidadesRomanas.Length; i++)
+                numeros.Add(unidadesRomanas[i], i);
+        }
+
+        private void ConfigurarCentenasRomanas(string[] centenasRomanas, string[] dezenasRomanas)
+        {
+            for (int i = 0; i < centenasRomanas.Length; i++)
+            {
+                var valorCentena = (i + 1) * 100;
+
+                numeros.Add(centenasRomanas[i], valorCentena);
+
+                for (int j = 0; j < dezenasRomanas.Length; j++)
+                {
+                    var valorCentenaDezena = ((j + 1) * 10) + valorCentena;
+                    numeros.Add(centenasRomanas[i] + dezenasRomanas[j], valorCentenaDezena);
+                }
+            }
+        }
+    }
+}
---
+++ ConversorNumeros.Testes/ConversorNumeroRomanoTest.cs
@@ -0,0 +1,319 @@
+using ConversorNumero.Dominio;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ConversorNumeros.Testes
+{
+    
+    [TestClass]
+    public class ConversorNumeroRomanoTest 
+    {
+        private ConversorNumeroRomano conversor;
+
+        public ConversorNumeroRomanoTest()
+        {
+            conversor = new ConversorNumeroRomano();
+        }
+
+        #region classe de equivalencia 01 - Unidades
+        [TestMethod]
+        [DataRow("I",1)]
+        [DataRow("II", 2)]
+        [DataRow("III", 3)]
+        [DataRow("IV", 4)]
+        [DataRow("V", 5)]
+        [DataRow("VI", 6)]
+        [DataRow("VII", 7)]
+        [DataRow("VIII", 8)]
+        [DataRow("IX", 9)]
+        [DataRow("X", 10)]                
+        public void Deve_converter_numeros_romanos_do_1_ao_10(string numero, int resultadoEsperado)
+        {
+            //cenário - arrange
+            string numeroRomano = numero;
+
+            //ação - action 
+            var resultado = conversor.ConverterParaArabico(numeroRomano);
+
+            //verificação - assert
+            Assert.AreEqual(resultadoEsperado, resultado);
+        }
+
+        #endregion
+
+        #region classe de equivalencia 02 - Dezena e Unidade
+        [TestMethod]
+        [DataRow("XI", 11)]
+        [DataRow("XII", 12)]
+        [DataRow("XIII", 13)]
+        [DataRow("XIV", 14)]
+        [DataRow("XV", 15)]
+        [DataRow("XVI", 16)]
+        [DataRow("XVII", 17)]
+        [DataRow("XVIII", 18)]
+        [DataRow("XIX", 19)]
+        [DataRow("XX", 20)]
+        public void Deve_converter_numeros_romanos_do_11_ao_20(string numero, int resultadoEsperado)
+        {
+            string numeroRomano = numero;
+
+            //ação - action 
+            var resultado = conversor.ConverterParaArabico(numeroRomano);
+
+            //verificação - assert
+            Assert.AreEqual(resultadoEsperado, resultado);
+        }
+
+
+        [TestMethod]
+        [DataRow("XXI", 21)]
+        [DataRow("XXII", 22)]
+        [DataRow("XXIII", 23)]
+        [DataRow("XXIV", 24)]
+        [DataRow("XXV", 25)]
+        [DataRow("XXVI", 26)]
+        [DataRow("XXVII", 27)]
+        [DataRow("XXVIII", 28)]
+        [DataRow("XXIX", 29)]
+        [DataRow("XXX", 30)]
+        public void Deve_converter_numeros_romanos_do_21_ao_30(string numero, int resultadoEsperado)
+        {
+            string numeroRomano = numero;
+
+            //ação - action 
+            var resultado = conversor.ConverterParaArabico(numeroRomano);
+
+            //verificação - assert
+            Assert.AreEqual(resultadoEsperado, resultado);
+        }
+
+        [TestMethod]
+        [DataRow("XXXI", 31)]
+        [DataRow("XXXII", 32)]
+        [DataRow("XXXIII", 33)]
+        [DataRow("XXXIV", 34)]
+        [DataRow("XXXV", 35)]
+        [DataRow("XXXVI", 36)]
+        [DataRow("XXXVII", 37)]
+        [DataRow("XXXVIII", 38)]
+        [DataRow("XXXIX", 39)]
+        [DataRow("XL", 40)]
+        public void Deve_converter_numeros_romanos_do_31_ao_40(string numero, int resultadoEsperado)
+        {
+            string numeroRomano = numero;
+
+            //ação - action 
+            var resultado = conversor.ConverterParaArabico(numeroRomano);
+
+            //verificação - assert
+            Assert.AreEqual(resultadoEsperado, resultado);
+        }
+
+        [TestMethod]
+        [DataRow("XLI", 41)]
+        [DataRow("XLII", 42)]
+        [DataRow("XLIII", 43)]
+        [DataRow("XLIV", 44)]
+        [DataRow("XLV", 45)]
+        [DataRow("XLVI", 46)]
+        [DataRow("XLVII", 47)]
+        [DataRow("XLVIII", 48)]
+        [DataRow("XLIX", 49)]
+        [DataRow("L", 50)]
+        public void Deve_converter_numeros_romanos_do_41_ao_50(string numero, int resultadoEsperado)
+        {
+            string numeroRomano = numero;
+
+            //ação - action 
+            var resultado = conversor.ConverterParaArabico(numeroRomano);
+
+            //verificação - assert
+            Assert.AreEqual(resultadoEsperado, resultado);
+        }
+
+        [TestMethod]
+        [DataRow("LI", 51)]
+        [DataRow("LII", 52)]
+        [DataRow("LIII", 53)]
+        [DataRow("LIV", 54)]
+        [DataRow("LV", 55)]
+        [DataRow("LVI", 56)]
+        [DataRow("LVII", 57)]
+        [DataRow("LVIII", 58)]
+        [DataRow("LIX", 59)]
+        [DataRow("LX", 60)]
+        public void Deve_converter_numeros_romanos_do_51_ao_60(string numero, int resultadoEsperado)
+        {
+            string numeroRomano = numero;
+
+            //ação - action 
+            var resultado = conversor.ConverterParaArabico(numeroRomano);
+
+            //verificação - assert
+            Assert.AreEqual(resultadoEsperado, resultado);
+        }
+
+        [TestMethod]
+        [DataRow("LXI", 61)]
+        [DataRow("LXII", 62)]
+        [DataRow("LXIII", 63)]
+        [DataRow("LXIV", 64)]
+        [DataRow("LXV", 65)]
+        [DataRow("LXVI", 66)]
+        [DataRow("LXVII", 67)]
+        [DataRow("LXVIII", 68)]
+        [DataRow("LXIX", 69)]
+        [DataRow("LXX", 70)]
+        public void Deve_converter_numeros_romanos_do_61_ao_70(string numero, int resultadoEsperado)
+        {
+            string numeroRomano = numero;
+
+            //ação - action 
+            var resultado = conversor.ConverterParaArabico(numeroRomano);
+
+            //verificação - assert
+            Assert.AreEqual(resultadoEsperado, resultado);
+        }
+
+        [TestMethod]
+        [DataRow("LXXI", 71)]
+        [DataRow("LXXII", 72)]
+        [DataRow("LXXIII", 73)]
+        [DataRow("LXXIV", 74)]
+        [DataRow("LXXV", 75)]
+        [DataRow("LXXVI", 76)]
+        [DataRow("LXXVII", 77)]
+        [DataRow("LXXVIII", 78)]
+        [DataRow("LXXIX", 79)]
+        [DataRow("LXXX", 80)]
+        public void Deve_converter_numeros_romanos_do_71_ao_80(string numero, int resultadoEsperado)
+        {
+            string numeroRomano = numero;
+
+            //ação - action 
+            var resultado = conversor.ConverterParaArabico(numeroRomano);
+
+            //verificação - assert
+            Assert.AreEqual(resultadoEsperado, resultado);
+        }
+
+
+        [TestMethod]
+        [DataRow("LXXXI", 81)]
+        [DataRow("LXXXII", 82)]
+        [DataRow("LXXXIII", 83)]
+        [DataRow("LXXXIV", 84)]
+        [DataRow("LXXXV", 85)]
+        [DataRow("LXXXVI", 86)]
+        [DataRow("LXXXVII", 87)]
+        [DataRow("LXXXVIII", 88)]
+        [DataRow("LXXXIX", 89)]
+        [DataRow("XC", 90)]
+        public void Deve_converter_numeros_romanos_do_81_ao_90(string numero, int resultadoEsperado)
+        {
+            string numeroRomano = numero;
+
+            //ação - action 
+            var resultado = conversor.ConverterParaArabico(numeroRomano);
+
+            //verificação - assert
+            Assert.AreEqual(resultadoEsperado, resultado);
+        }
+
+        [TestMethod]
+        [DataRow("XCI", 91)]
+        [DataRow("XCII", 92)]
+        [DataRow("XCIII", 93)]
+        [DataRow("XCIV", 94)]
+        [DataRow("XCV", 95)]
+        [DataRow("XCVI", 96)]
+        [DataRow("XCVII", 97)]
+        [DataRow("XCVIII", 98)]
+        [DataRow("XCIX", 99)]
+        [DataRow("C", 100)]
+        public void Deve_converter_numeros_romanos_do_91_ao_100(string numero, int resultadoEsperado)
+        {
+            string numeroRomano = numero;
+
+            //ação - action 
+            var resultado = conversor.ConverterParaArabico(numeroRomano);
+
+            //verificação - assert
+            Assert.AreEqual(resultadoEsperado, resultado);
+        }
+
+        #endregion
+
+        #region classe de equivalencia 03 - Centena, Dezena e Unidade
+        [TestMethod]
+        [DataRow("CI", 101)]
+        [DataRow("CII", 102)]
+        [DataRow("CIII", 103)]
+        [DataRow("CIV", 104)]
+        [DataRow("CV", 105)]
+        [DataRow("CVI", 106)]
+        [DataRow("CVII", 107)]
+        [DataRow("CVIII", 108)]
+        [DataRow("CIX", 109)]
+        [DataRow("CX", 110)]
+        public void Deve_converter_numeros_romanos_do_101_ao_110(string numero, int resultadoEsperado)
+        {
+            string numeroRomano = numero;
+
+            //ação - action 
+            var resultado = conversor.ConverterParaArabico(numeroRomano);
+
+            //verificação - assert
+            Assert.AreEqual(resultadoEsperado, resultado);
+        }
+
+        [TestMethod]
+        [DataRow("CXI", 111)]
+        [DataRow("CXII", 112)]
+        [DataRow("CXIII", 113)]
+        [DataRow("CXIV", 114)]
+        [DataRow("CXV", 115)]
+        [DataRow("CXVI", 116)]
+        [DataRow("CXVII", 117)]
+        [DataRow("CXVIII", 118)]
+        [DataRow("CXIX", 119)]
+        [DataRow("CXX", 120)]
+        public void Deve_converter_numeros_romanos_do_111_ao_120(string numero, int resultadoEsperado)
+        {
+            string numeroRomano = numero;
+
+            //ação - action 
+            var resultado = conversor.ConverterParaArabico(numeroRomano);
+
+            //verificação - assert
+            Assert.AreEqual(resultadoEsperado, resultado);
+        }
+
+        #endregion
+
+        #region classe de equivalencia 04 - Milhar, Centena, Dezena e Unidade
+
+        [TestMethod]
+        [DataRow("MI", 1001)]
+        [DataRow("MII", 1002)]
+        [DataRow("MIII", 1003)]
+        [DataRow("MIV", 1004)]
+        [DataRow("MV", 1005)]
+        [DataRow("MVI", 1006)]
+        [DataRow("MVII", 1007)]
+        [DataRow("MVIII", 1008)]
+        [DataRow("MIX", 1009)]
+        [DataRow("MX", 1010)]
+        public void Deve_converter_numeros_romanos_do_1001_ao_1010(string numero, int resultadoEsperado)
+        {
+            string numeroRomano = numero;
+
+            //ação - action 
+            var resultado = conversor.ConverterParaArabico(numeroRomano);
+
+            //verificação - assert
+            Assert.AreEqual(resultadoEsperado, resultado);
+        }
+
+        #endregion
+    }
+}
